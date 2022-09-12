81a22ca0f87b289e9fd0db40dab543d216c406e5
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2022-09-12 11:21:24 +0300

    added error handling

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,43 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from './post.service';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from './post.service';
 
-@Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css'],
-})
+@Component({selector: 'app-posts', templateUrl: './posts.component.html', styleUrls: ['./posts.component.css']})
 export class PostsComponent implements OnInit {
-  posts: any[] = [];
+    posts : any[] = [];
 
-  constructor(private service: PostService) {
-  }
+    constructor(private service : PostService) {}
 
-  ngOnInit(){
-    this.service.getPosts().subscribe((response) => {
-      this.posts= response as any[];
-    })
-  }
+    ngOnInit() {
+        this.service.getPosts()
+        .subscribe(
+          response => {
+            this.posts = response as any[];
+        },
+        error => {
+            alert('An unexpected error occurred.');
+            console.log(error);
+        })
+    }
 
-  createPost(titleInput: HTMLInputElement) {
-    let post: any = { title: titleInput.value };
-    titleInput.value = '';
-    this.service.createPost(post).subscribe((response) => {
-        post.id = response;
-        this.posts.splice(0, 0, post);
-      });
-  }
+    createPost(titleInput : HTMLInputElement) {
+        let post: any = {
+            title: titleInput.value
+        };
+        titleInput.value = '';
 
-  updatePost(post: any) {
-    this.service.updatePost().subscribe((response) => {
-      console.log(response);
-    })
-  }
+        this.service.createPost(post)
+        .subscribe(
+          response => {
+            post.id = response;
+            this.posts.splice(0, 0, post);
+        },
+        (error: Response) => {
+          if(error.status === 400){
+            //this.form.setErrors(error.json());
+          }
+          else {
+            alert('An unexpected error occurred.');
+            console.log(error);
+          }
+        });
+    }
 
-  deletePost(post: any) {
-    this.service.deletePost(post.id).subscribe((response) => {
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      });
-  }
+    updatePost(post : any) {
+        this.service.updatePost()
+        .subscribe(
+          response => {
+            console.log(response);
+        },
+        error => {
+            alert('An unexpected error occurred.');
+            console.log(error);
+        })
+    }
+
+    deletePost(post : any) {
+        this.service.deletePost(post.id)
+        .subscribe(
+          response => {
+            let index = this.posts.indexOf(post);
+            this.posts.splice(index, 1);
+        },
+        (error: Response) => {
+          if(error.status === 404)
+            alert('This post has already been deleted.');
+          else {
+            alert('An unexpected error occurred.');
+            console.log(error);
+          }
+        });
+    }
 
 }
+

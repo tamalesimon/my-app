e0185ee78cd4b6e0af70d11577ee8b59b2a50d97
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2022-10-04 10:46:49 +0300

    concept of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './courses.component';
 import { CourseFormComponent } from './course-form/course-form.component';
@@ -17,6 +18,13 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostService } from './posts/post.service';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -32,7 +40,13 @@ import { PostService } from './posts/post.service';
     ContactFormComponent,
     SignupFormComponent,
     NewCourseFormComponent,
-    PostsComponent
+    PostsComponent,
+    GithubFollowersComponent,
+    GithubProfileComponent,
+    NotFoundComponent,
+    ZippyComponent,
+    LikeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -41,6 +55,13 @@ import { PostService } from './posts/post.service';
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'followers', component: GithubFollowersComponent},
+      {path:'profile/:username', component: GithubProfileComponent},
+      {path:'posts', component: PostsComponent},
+      {path:'**', component: NotFoundComponent}
+    ])
   ],
   providers: [
     CoursesService, //dependency injection registration
---
+++ src/app/services/github-followers.service.ts
@@ -0,0 +1,11 @@
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GithubFollowersService extends DataService {
+
+  constructor(http: HttpClient) {
+    super('https://api.github.com/users/mosh-hamedani/followers', http);
+  }
+}
